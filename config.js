9c24261548f9bf88cf5e66cde1992684f0e71ec0
logicminer <38355433+logicminer@users.noreply.github.com>
2018-04-13 22:30:25 +0800

    Initial commit

---
+++ config.js
@@ -0,0 +1,23 @@
+define( function ( require ) {
+
+	"use strict";
+
+	return {
+		app_slug : 'team-destiny',
+		wp_ws_url : 'https://teamdestiny.win/wp-appkit-api/team-destiny',
+		wp_url : 'https://teamdestiny.win',
+		theme : 'q-android',
+		version : '1.0',
+		app_type : 'phonegap-build',
+		app_title : '',
+		app_platform : 'android',
+		app_path: '',
+		gmt_offset : 0,
+		debug_mode : 'off',
+		auth_key : 'f7grJxT,aiRTAZu>S`>$IH`=[%XY${vw21o9F$`Et&?gat}!]U7.tc#32zbC<%[2',
+		options : {"refresh_interval":0},
+		theme_settings : [],
+		addons : []
+	};
+
+});
